d7943ece0b638c3bd98156819d9d57729cd46ad0
Greg Loesch <gloesch@weddingwire.com>
2014-06-24 11:04:22 -0400

    Add ability to transition activities on button click [session3]
    - Add a listener to the button which will create and use an intent to launch the new activity.
    - Pass data through the intent to the new activity.

--- app/src/main/java/com/weddingwire/wedblog/HomeActivity.java
+++ app/src/main/java/com/weddingwire/wedblog/HomeActivity.java
@@ -1,14 +1,47 @@
 package com.weddingwire.wedblog;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity implements View.OnClickListener {
+	private static final String TAG = HomeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+	    Log.d(TAG, "onCreate");
+
+	    ImageButton imageButton = (ImageButton) findViewById(R.id.feather);
+	    imageButton.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.feather:
+				startCreateBlogPost();
+				break;
+			default:
+				throw new RuntimeException("Unknown button click!");
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume");
+	}
+
+	private void startCreateBlogPost() {
+		Intent intent = new Intent(this, CreateBlogActivity.class);
+		intent.putExtra(CreateBlogActivity.EXTRA_NAME, "this is a test");
+		startActivity(intent);
+	}
+
 }
