b5180b5f8c30d77eacc5dd18a03303c6f168c839
Greg Loesch <gloesch@weddingwire.com>
2014-07-07 16:17:04 -0400

    Add PostListActivity [list]
    - Add PostListActivity and add a ListView in the view xml. This will hold the blog posts.

---
+++ app/src/main/java/com/weddingwire/wedblog/activities/PostListActivity.java
@@ -0,0 +1,16 @@
+package com.weddingwire.wedblog.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.weddingwire.wedblog.R;
+
+public class PostListActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_list);
+    }
+
+}
